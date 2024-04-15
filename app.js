af7e2fda314da1039b0754e53b4d91a3e44f29db
umairaslam2 <154333213+umairaslam2@users.noreply.github.com>
2024-04-15 20:19:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,214 @@
+//h
+//""
+//g
+
+
+//chapter4
+
+//question # 01;
+
+// var abc1 = 21;
+// var abc2 = 12;
+// var abc3 = 21;
+
+//question # 02;
+
+//5 illegal variable names;
+
+//1. var 123 =
+//2. var 1st =
+//3. var alert =
+//4. var full name =
+//5. var Name =
+
+
+//5 legal variable names;
+
+//1. var abc1 = 
+//2. var abc2 = 
+//3. var abc3 =
+//4. var fullName =
+//5. var abc4 =
+
+
+//question # 03;
+
+//a) “Rules for naming JS variables”
+
+//b) Variable names can only contain letter numbers, dollar signs, and underscores
+//For example $my_1stVariable
+
+//c) Variables must begin with a letter,
+// For example $name, _name or name
+
+//d) Variable names are case sensitive
+
+//e) Variable names should not be JS keyword
+
+
+
+
+//chapter5
+
+//question # 01;
+
+//  var abc1 = 5;
+//  var abc2 = 3;
+
+//  console.log(abc1 + abc2);
+
+
+//question # 02;
+
+// var abc1 = 5;
+//  var abc2 = 3;
+
+//  console.log(abc1 - abc2);
+
+//  var abc1 = 5;
+//  var abc2 = 3;
+
+//  console.log(abc1 * abc2);
+
+//  var abc1 = 5;
+//  var abc2 = 3;
+
+//  console.log(abc1 / abc2);
+
+
+//question # 03;
+
+// var abc1 = 5;
+// var abc2 = 7;
+// var abc3 = 1;
+// console.log(++abc1 + abc2 - abc3 );
+
+
+//question # 04;
+
+// var ticket = 600
+
+// console.log(ticket * 5);
+
+//question # 07;
+
+// var item1 = 650;
+// var item2 = 100;
+// var quantity1 = 3;
+// var quantity2 = 7;
+// var Shipping = 100;
+
+// console.log(item1 * quantity1 + item2 * quantity2 + Shipping);
+
+
+//question # 08;
+
+// var total = 980;
+// var obtained = 804
+
+// console.log(obtained*100 /total);
+
+//question # 09;
+
+// var usDollers = 10;
+// var riyal = 25;
+
+// console.log(usDollers * 104.80 + riyal * 28);
+
+
+
+//question # 10;
+
+// var abc1 = 5;
+// var abc2 = 10;
+// var abc3 = 2;
+
+// console.log(abc1*abc2/abc3)
+
+//question # 11;
+
+// var currentYear = 2016;
+// var birtYear = 1992;
+
+// console.log(currentYear-birtYear);
+
+//question 13;
+
+//var snack = abc;
+// var currentAge = 20;
+// var estimatedAge = 70;
+// var amountPerDay = 3;
+// var result =estimatedAge-currentAge
+
+// console.log(result * amountPerDay);
+
+
+
+//chapter 6-9
+
+//question 1;
+
+// var abc = 10;
+
+// console.log(abc);
+
+// console.log(++abc);
+
+// console.log(++abc);
+
+// console.log(--abc);
+
+// console.log(--abc);
+
+
+// question 2;
+
+// var a = 2;
+// var b = 1;
+
+// var result = --a - --b + ++b + b--;
+//              1  -  0 +   1 + 1
+
+//            console.log(result);
+
+// question 6;
+
+// var subject1 = "Audit";
+// var subject2 = "Tax";
+// var subject3 = "Blaw";
+
+// var total = 100;
+// var obtained1 = 54;
+// var obtained2 = 54;
+// var obtained3 = 48;
+// var totalMark = 300
+// var marksObtaioned = 156;
+
+// console.log("total Marks" +  " = "  + total *3)
+
+// console.log(subject1 + "=" + obtained1 + "%");
+
+// console.log(subject2 + "=" + obtained2 + "%");
+
+// console.log(subject3 + "=" + obtained3 + "%");
+
+// console.log("Total%" + "=" + marksObtaioned/totalMark + "%")
+
+// question 3
+
+// var abc = prompt("Enter your name");
+
+// alert("Salam");
+
+
+
+
+
+
+
+
+
+
+
+
+
